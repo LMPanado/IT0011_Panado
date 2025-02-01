576b8783ed86d58e6d3208c4dc5af496a7ca90f5
LMPanado <lukemendozapanado@gmail.com>
2025-02-01 14:42:24 +0800

    Panado_TA1

---
+++ TA1/TA1.2.py
@@ -0,0 +1,6 @@
+total = 0
+input_string = input ("Enter a string containing digits: ")
+for char in input_string:
+     if char.isdigit():
+         total += int(char)
+print ("The sum of digits is: ", total)
